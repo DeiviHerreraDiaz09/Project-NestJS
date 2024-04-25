5ee7cce9bb70c0293e4a57e9cdcf07026ba20621
DeiviHerreraDiaz09 <herreradiazdeivistiven2004@gmail.com>
2024-04-25 15:58:18 -0500

    FEAT: Creación Arquitectura Modular v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { UsersController } from './users/controller/users.controller';
-import { StoresController } from './stores/controller/stores.controller';
-import { ProductsController } from './products/controller/products.controller';
-import { CategoriesController } from './categories/controller/categories.controller';
-import { UsersService } from './users/services/users.service';
-import { ProductsService } from './products/services/products.service';
-import { CategoriesService } from './categories/services/categories.service';
-import { StoresService } from './stores/services/stores.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
@@ -15,7 +7,7 @@ import { StoresModule } from './stores/stores.module';
 
 @Module({
   imports: [UsersModule, ProductsModule, CategoriesModule, StoresModule],
-  controllers: [AppController, ProductsController, CategoriesController, UsersController, StoresController],
-  providers: [UsersService, ProductsService, CategoriesService, StoresService], 
+  controllers: [AppController],
+  providers: [], 
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './controller/users.controller'
+import { UsersService } from './services/users.service'
 
-@Module({})
+
+@Module({
+    controllers: [UsersController],
+    providers: [UsersService]
+})
 export class UsersModule {}
